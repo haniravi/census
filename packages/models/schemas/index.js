4e27a982fe55171eea4bc38aaace73a2d5edf9c3
Miro <miro.ter.seri@gmail.com>
2019-05-31 00:44:56 +0400

    :golf: conform schemas and mocks via tests
    Simplify both mocks and schemas

--- packages/models/schemas/index.js
+++ packages/models/schemas/index.js
@@ -1,5 +1,9 @@
-const entity = require('./entity.json')
+const glossary = require('./glossary.json')
+const person = require('./person.json')
+const organization = require('./organization.json')
 
 module.exports = [
-  entity
+  glossary,
+  person,
+  organization
 ]
